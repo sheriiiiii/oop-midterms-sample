607daad88d4f82ad5336b3e1f2b77a4e0998b7c2
Valerie Grace Q. Aaron <124411954+Eeeeeyron18@users.noreply.github.com>
2023-04-25 21:53:21 +0800

    Update Aaron-Pangilinan-Laluma-OOP-Midterms.ts

--- Aaron-Pangilinan-Laluma-OOP-Midterms.ts
+++ Aaron-Pangilinan-Laluma-OOP-Midterms.ts
@@ -4,10 +4,13 @@ abstract class Restaurant {
     private _openTime: number;
     private _closeTime: number;
     protected cuisine: string;
-    protected menu: string[];
-    protected rating: number = 0;
+    protected menu: string;
+    protected rating: number;
+    private _franchise: boolean;
+    private _shareholderStocks: void;
+    private _marketing: void;
 
-    constructor(name: string, location: string, openTime: number, closeTime: number, cuisine: string, menu: string[], rating: number) {
+    constructor(name: string, location: string, openTime: number, closeTime: number, cuisine: string, menu: string, rating: number, franchise: boolean, shareholderStocks: void, marketing: void) {
         this._name = name;
         this._location = location;
         this._openTime = openTime;
@@ -15,64 +18,48 @@ abstract class Restaurant {
         this.cuisine = cuisine;
         this.menu = menu;
         this.rating = rating;
-    };
-
-    public getRestoName(): string {
-        return this._name;
-    };
-
-    public hoursOpen(): string {
-        return `The ${this._name} is open from ${this._openTime} to ${this._closeTime}.`;
-    };
-
-    public getMenuItem(): string[] {
-        return this.menu;
-    };
-
-    public takeOrder(order: string): void {
-        console.log(`We got you order of ${order}. We'll have it ready for you shortly.' `);
-    };
-
-    getPayment(amount: number): void  {
-        console.log(`Payment of ${amount} received. Thank you for dining at ${this._name}. Please come again!`);
-    };
-
-    getFeedback(feedback: string): void {
-        if (this.rating  <= 5 && feedback === "") {
-            console.log(`We've received ${this.rating} stars: ${feedback}. Your honest review will greatly help us improve our services. Thank you and have a great day.`);
-        } else {
-            console.log(`We've received ${this.rating} stars: ${feedback}. Your honest review will greatly help us improve our services. Thank you and have a great day.`);
-        };
-    };
-}
-
-    class Cafe extends Restaurant {
-        private _franchise: string
-        private _shareholderStocks: string
-        private _marketing: string;
-
-        constructor(name: string, location: string, openTime: number, closeTime: number, cuisine: string, menu: string[], rating: number, franchise: string, shareholderStocks: string, marketing: string) {
-        super(name, location, openTime, closeTime, cuisine, menu, rating, franchise, shareholderStocks, marketing);
         this._franchise = franchise;
         this._shareholderStocks = shareholderStocks;
         this._marketing = marketing;
     };
 
-    public sellStocks(): {
+    private getRestoName(): string {
+        
+    };
+
+    private hoursOpen(): void {
+        
+    };
+
+    public getMenuItem(): void {
 
     };
 
-    public buyStocks(): {
+    public takeOrder(): void {
 
     };
 
-    public expandFranchise(): {
+    public getPayment(): void {
 
     };
 
-    public audienceReached(): {
+    public getFeedback(): void {
 
     };
+
+    public sellStock(): void {
+
     };
 
+    public buyStocks(): void {
 
+    };
+
+    public expandFranchise(): void {
+
+    };
+
+    public audienceReached(): void {
+
+    };
+};
