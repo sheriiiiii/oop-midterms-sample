57d1a3147a972b2ffe5db7a836d2986c6326e0f1
sheriiiiii <124129869+sheriiiiii@users.noreply.github.com>
2023-04-26 21:58:56 +0800

    Update Aaron-Pangilinan-Laluma-OOP-Midterms.ts

--- Aaron-Pangilinan-Laluma-OOP-Midterms.ts
+++ Aaron-Pangilinan-Laluma-OOP-Midterms.ts
@@ -1,9 +1,11 @@
 interface Business {
-    getRestoName(): string;
+    getRestoName(): string;//
+    getRestoInfo(email: string): void;
     getMenuItem(): void;
     takeOrder(order: string): void;
     getPayment(): void;
-    getPaymentMethod(): void;
+    getPaymentMethod(): void;//
+    getAvailableTables(): void;//
     hoursOpen(): void;
     getRatings(): void;
     getFeedback(): void;
@@ -14,20 +16,23 @@ interface Business {
         protected _location: string;
         protected _cuisine: string;
         protected _menu: string[];
+        protected _orderList: string[];
         protected _paymentMethod: string;
+        protected _availableTables: number;
         protected _openTime: string;
         protected _closeTime: string;
         protected _rating: number;
-        //protected _email: void;
+        
 
-
-        constructor(name: string, location: string, cuisine: string, menu: string[], 
-            paymentMethod: string, openTime: string, closeTime: string, rating: number, ) {
+        constructor(name: string, location: string, cuisine: string, menu: string[], orderList: string[],
+            paymentMethod: string, availableTables: number, openTime: string, closeTime: string, rating: number) {
             this._name = name;
             this._location = location;
             this._cuisine = cuisine;
             this._menu = menu;
+            this._orderList = orderList;
             this._paymentMethod = paymentMethod;
+            this._availableTables = availableTables;
             this._openTime = openTime;
             this._closeTime = closeTime;
             this._rating = rating;
@@ -36,6 +41,10 @@ interface Business {
         public getRestoName(): string {
             return this._name;
         };
+
+        public getRestoInfo(email: string): void {
+            console.log(`Hi there! This is ${this._name}. We are located at ${this._location}. If you have any concerns, please contact us on our email: ${email}`)
+        }
 
         public getMenuItem(): void {
             console.log(`Welcome! Today's menu features ${this._cuisine}, including ${this._menu}`);
@@ -75,10 +84,14 @@ interface Business {
             console.log(`${this._name} is open for ${time} hours. Enjoy!`);
         };
 
+        public getAvailableTables(): void {
+            console.log(`There are ${this._availableTables} tables available.`)
+        }
+
         public getRatings(): void {
-            if (this._rating === 5 || this._rating === 4) {
+            if (this._rating = 5) {
                 console.log(`Thank you for rating us ${this._rating} stars!`);
-            } else if (this._rating === 3) {
+            } else if (this._rating >= 3) {
                 console.log(`Thank you for rating us ${this._rating} stars! We will improve our service for your best experience.`)
             } else if (this._rating < 3) {
                 console.log(`We are sorry that we didn't meet your expectations. Thank you for your review.`)
@@ -92,17 +105,18 @@ interface Business {
     };
 
     class FastFood extends Restaurant {
-        private _customerLocation: string;
         private _deliveryRider: string;
+        private _orderID: string;
+        private _queNumber: string;
 
-
-
-        constructor(name: string, cuisine: string, menu: string[], 
-            paymentMethod: string, openTime: string, closeTime: string, rating: number, 
-            location: string, customerLocation: string, deliveryRider: string) {
-        super(name, location, cuisine, menu, paymentMethod, openTime, closeTime, rating) 
-            this._customerLocation = customerLocation;
+        constructor(name: string, location: string, cuisine: string, menu: string[], orderList: string[],
+            paymentMethod: string, availableTables: number, openTime: string, closeTime: string, rating: number, 
+            deliveryRider: string, orderID: string, queNumber: string) {
+        super(name, location, cuisine, menu, orderList, paymentMethod, availableTables, openTime, closeTime, rating) 
+            
             this._deliveryRider = deliveryRider;
+            this._orderID = orderID;
+            this._queNumber = queNumber;
         };
 
         public getRestoName(): string {
@@ -110,23 +124,112 @@ interface Business {
         };
 
         public getMenuItem(): void {
-            console.log(`Welcome! Today's menu features ${this._cuisine}, including ${this._menu}`);
+            console.log(`Welcome to ${this._name}! Here are is the menu: ${this._menu}.`);
         };
 
         public takeOrder(order: string): void {
             if (this._menu.includes(order)) {
-                console.log(`We got you order of ${order}. We'll have it ready for you shortly.`);
+                this._orderList.push(order);
+                console.log(`We got you order of ${this._orderList}. We'll have it ready for you shortly.`);
             } else {
                 console.log(`Sorry your order of ${order} is not available.`);
             };
         };
 
-        public getPayment(): void {
-            console.log(`Please pay at the cashier. Thank you.`);
-        };
-
-        public deliveryService(): void {
-            console.log(`Delivery is on the way by rider ${this._deliveryRider}`)
+        public addItemToMenu (item: string): void {
+            this._orderList.push(item);
+            console.log(`You added ${item} to menu.`)
         }
 
+        public getCustomerInfo(customerName: string, customerLocation: string): void {
+            console.log(`Hi ${customerName}! Your order is on the way to ${customerLocation}.`)
+        }
+
+        public deliveryService(): void {
+            console.log(`Hello there! Your order will be delivered by ${this._deliveryRider}.`)
+        }
+
+        public getOrderID(): void {
+            console.log(`Your order ID is ${this._orderID}.`)
+        }
+
+        public driveThru(): void {
+            console.log(`Please wait in line. Your que number is ${this._queNumber}`)
+        }
     }
+
+    class FineDining extends Restaurant {
+
+        constructor(name: string, location: string, cuisine: string, menu: string[], orderList: string[],
+            paymentMethod: string, availableTables: number, openTime: string, closeTime: string, rating: number, 
+            ) {
+        super(name, location, cuisine, menu, orderList, paymentMethod, availableTables, openTime, closeTime, rating) 
+            
+            
+        };
+        public hoursOpen(): void {
+            let openHour = parseInt(this._openTime.split(':')[0]);
+            let closeHour = parseInt(this._closeTime.split(':')[0]);
+            let time: number;
+
+            if (openHour <= closeHour) {
+                time = closeHour - openHour;
+              } else {
+                time = 24 - openHour + closeHour;
+              };
+
+            console.log(`${this._name} is open for ${time} hours. Enjoy your fine dining experience!`);
+        };
+
+        public makeReservation(): void {
+            if (this._availableTables == this._availableTables) {
+                console.log(`You can't make a reservation now.`)
+            } else {
+                console.log(`Reservation is set. Enjoy!`)
+            }
+        }
+
+        public prepareMeal(meal: string): void {
+            console.log(`${meal} will be prepared in a short while.`)
+        }
+        
+        public serveMeal(customerName: string, meal: string): void {
+            if (this._menu.includes(meal)) {
+                console.log(`Serving ${customerName} a ${meal}.`)
+            } else {
+                console.log(`Dish not available on menu.`)
+            }
+        }
+
+        public getMealInfo(executiveChef: string): void {
+            console.log(`Welcome to ${this._name}! For today's menu we will offer you ${this._cuisine} cuisine headed by the head chef ${executiveChef}`)
+        }
+
+        public wineOption(wine: string): void {
+            if (wine == 'Red Wine' || wine == 'White Wine' || wine == 'Rose Wine') {
+                console.log('Your ${wine} will pe prepared.')
+            } else {
+                console.log('Your choice is not available.')
+            }
+        }
+    }
+
+const mcdo: FastFood = new FastFood("McDonald's", "Ungka, Pavia", "N/A", 
+["Burger", "Fries", "Chicken", "Sundae", "Soda"], [], "cash", 15, "9:00", "21:00", 3, "Juan dela Cruz", "ID306", "15"   )
+
+mcdo.addItemToMenu("Spaghetti");
+mcdo.takeOrder("Burger");
+mcdo.getCustomerInfo("Valerie Aaron", "Jaro");
+mcdo.getAvailableTables();
+
+const sheries: FineDining = new FineDining("Sherie's", "N/A", "French", 
+["Boeuf Bourguignon", "Moules Marinières", "Blanquette de Veau", "Soupe à L’oignon"], 
+[],"credit card", 10, "18:00", "22:00", 5);
+
+sheries.hoursOpen();
+sheries.getPaymentMethod();
+sheries.getMealInfo("Chef Paul Bocuse");
+sheries.makeReservation();
+sheries.prepareMeal("Boeuf Bourguignon");
+sheries.prepareMeal("Chicken Biryani");
+
